635e458688b03ae39b32a7601bcfb7656618245d
Gorielov <103457728+Gorielov@users.noreply.github.com>
2022-09-19 19:57:26 +0000

    Deploying to gh-pages from @ Gorielov/sprite-test-project@663c344ece1e7bc7ef65af204dbc679144ed332c 🚀

---
+++ index.041c79e7.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},grabCursor:!0,keyboard:{enabled:!0,onlyInViewport:!0},mousewheel:{sensitivity:1},loop:!0});
+//# sourceMappingURL=index.041c79e7.js.map
